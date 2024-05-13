84454dd570f7a0ed599190e5a06b4e2e6489242c
igorspestana <igorspestana@gmail.com>
2024-05-13 19:59:04 -0300

    feat: added animation with lottie

--- README.md
+++ README.md
@@ -9,13 +9,28 @@ Para utilizar com React Native CLI instalar:
 yarn add react-native-vector-icons
 ```
 
-Para utiliza com o Expo já basta utilzar @expo/vector-icons:
+Para utilizar com o Expo já basta utilzar @expo/vector-icons:
 
 Esta biblioteca é uma camada de compatibilidade em torno de @oblador/react-native-vector-icons para trabalhar com o sistema de ativos Expo.
 https://www.npmjs.com/package/@expo/vector-icons
-https://github.com/oblador/react-native-vector-icons
+
 
 
 ## LottieFiles
 Animações
 https://lottiefiles.com/pt/
+
+
+Fazer download da animação em formato json, colocar no diretório assets e apontar o endereço no component LottieView
+
+Para utilizar com React Native CLI:
+https://github.com/lottie-react-native/lottie-react-native
+
+Para utilizar com o Expo:
+https://docs.expo.dev/versions/latest/sdk/lottie/
+
+Instalar:
+
+```
+npx expo install lottie-react-native
+```
---
+++ icons-animations/app/index.tsx
@@ -0,0 +1,56 @@
+import React, { useRef, useEffect } from 'react';
+import { Text, View, Button, StyleSheet, } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import LottieView from 'lottie-react-native';
+
+export default function Index() {
+
+  const animation = useRef(null);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Ícone com expo/vector-icons</Text>
+      <Feather name="play" size={60} color="black" />
+
+      <Text>Ícone com expo/vector-icons</Text>
+      <LottieView
+        autoPlay
+        ref={animation}
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: '#eee',
+        }}
+        // Find more Lottie files at https://lottiefiles.com/featured
+        source={require('../assets/check-animation.json')}
+      />
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Restart Animation"
+          onPress={() => {
+            animation.current?.reset();
+            animation.current?.play();
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  animationContainer: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  buttonContainer: {
+    paddingTop: 20,
+  },
+});
